6869c0907ef83ba82ee09cd7e357ef6ad0faf850
splincode <omaxphp@yandex.ru>
2024-10-23 21:03:51 +0300

    chore: update in 09:03 pm

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
+if (process.env.VERCEL) {
+    // We are using Vercel, so we have to trigger a read of the static files
+    // needed by MockPass so that Vercel knows that we keep them at runtime
+    const fs = require('fs')
+    const path = require('path')
+
+    const pathPrefix = path.resolve(__dirname, '../node_modules/@opengovsg/mockpass/static')
+    fs.readdirSync(pathPrefix).map(dir => fs.readdirSync(path.resolve(pathPrefix, dir)))
+}
+
 app.post('/template', async (req, res) => {
    try {
        const body = req.body;
